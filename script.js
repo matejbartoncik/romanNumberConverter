7d31029b9ec88ca415d05f8bdf7d9e09677d7537
MistrMatej <matejbartoncik@seznam.cz>
2024-01-24 12:29:53 +0100

    Add emptyChecker function to validate input

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const btn = document.getElementById("convert-btn")
 const output = document.getElementById("output");
 const intputValue = document.getElementById("number");
-
+function emptyChecker() {
+    if (intputValue.value = "") {
+        output.innerHTML = "Please enter a valid number";
+    }
+}
+btn.addEventListener("click",emptyChecker())
